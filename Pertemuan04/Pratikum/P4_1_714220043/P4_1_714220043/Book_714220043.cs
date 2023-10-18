6b39e587b4d0bbf925ad5f60cee1dfa0ba45df11
Yoginara Pratama Sitorus <114574855+Yoginara@users.noreply.github.com>
2023-10-19 03:14:33 +0700

    Pratikum_4_1_714220043

---
+++ Pertemuan04/Pratikum/P4_1_714220043/P4_1_714220043/Book_714220043.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P4_1_714220043
+{
+    public class Book_714220043: Product_714220043 
+    {
+        protected string pageCount;
+
+        public Book_714220043(string type, string title, string pagecount) : base(type,title)
+        {
+            this.pageCount = pagecount;
+        }
+
+        public string PageCount
+        {
+            get
+            {
+                return pageCount;
+            }
+            set
+            {
+                pageCount = value;
+            }
+        }
+    }
+}
