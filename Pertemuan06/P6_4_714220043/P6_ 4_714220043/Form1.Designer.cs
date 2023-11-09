f24fa22c04a30515a05a5208ddfe5e8156b5ab5f
Yoginara Pratama Sitorus <114574855+Yoginara@users.noreply.github.com>
2023-11-10 01:43:38 +0700

    update

---
+++ Pertemuan06/P6_4_714220043/P6_ 4_714220043/Form1.Designer.cs
@@ -0,0 +1,305 @@
+﻿namespace P6__4_714220043
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.buttonPesan = new System.Windows.Forms.Button();
+            this.buttonBatal = new System.Windows.Forms.Button();
+            this.textBoxNama = new System.Windows.Forms.TextBox();
+            this.epCorrect = new System.Windows.Forms.ErrorProvider(this.components);
+            this.epWarning = new System.Windows.Forms.ErrorProvider(this.components);
+            this.epWrong = new System.Windows.Forms.ErrorProvider(this.components);
+            this.comboBoxRasa = new System.Windows.Forms.ComboBox();
+            this.textBoxPonsel = new System.Windows.Forms.TextBox();
+            this.numericUpDownJumlah = new System.Windows.Forms.NumericUpDown();
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.textBoxAlamat = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBoxPromo = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.epCorrect)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epWarning)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epWrong)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownJumlah)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.Location = new System.Drawing.Point(268, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(219, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Pemesanan Pizza";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(125, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nama Pemesan";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(125, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(121, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Jumlah Pesanan";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(125, 186);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Rasa";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(125, 225);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(104, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Nomor ponsel";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(124, 367);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 16);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Alamat";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(124, 269);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(46, 16);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Email";
+            // 
+            // buttonPesan
+            // 
+            this.buttonPesan.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonPesan.Location = new System.Drawing.Point(204, 446);
+            this.buttonPesan.Name = "buttonPesan";
+            this.buttonPesan.Size = new System.Drawing.Size(141, 48);
+            this.buttonPesan.TabIndex = 8;
+            this.buttonPesan.Text = "Pesan Sekarang";
+            this.buttonPesan.UseVisualStyleBackColor = true;
+            this.buttonPesan.Click += new System.EventHandler(this.buttonPesan_Click);
+            // 
+            // buttonBatal
+            // 
+            this.buttonBatal.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonBatal.Location = new System.Drawing.Point(399, 446);
+            this.buttonBatal.Name = "buttonBatal";
+            this.buttonBatal.Size = new System.Drawing.Size(141, 48);
+            this.buttonBatal.TabIndex = 9;
+            this.buttonBatal.Text = "Batal";
+            this.buttonBatal.UseVisualStyleBackColor = true;
+            this.buttonBatal.Click += new System.EventHandler(this.buttonBatal_Click_1);
+            // 
+            // textBoxNama
+            // 
+            this.textBoxNama.Location = new System.Drawing.Point(338, 91);
+            this.textBoxNama.Name = "textBoxNama";
+            this.textBoxNama.Size = new System.Drawing.Size(294, 22);
+            this.textBoxNama.TabIndex = 10;
+            this.textBoxNama.Leave += new System.EventHandler(this.textBoxNama_Leave);
+            // 
+            // epCorrect
+            // 
+            this.epCorrect.ContainerControl = this;
+            this.epCorrect.Icon = ((System.Drawing.Icon)(resources.GetObject("epCorrect.Icon")));
+            // 
+            // epWarning
+            // 
+            this.epWarning.ContainerControl = this;
+            this.epWarning.Icon = ((System.Drawing.Icon)(resources.GetObject("epWarning.Icon")));
+            // 
+            // epWrong
+            // 
+            this.epWrong.ContainerControl = this;
+            this.epWrong.Icon = ((System.Drawing.Icon)(resources.GetObject("epWrong.Icon")));
+            // 
+            // comboBoxRasa
+            // 
+            this.comboBoxRasa.FormattingEnabled = true;
+            this.comboBoxRasa.Items.AddRange(new object[] {
+            "Margherita",
+            "Pepperoni",
+            "Hawaiian (dengan potongan nanas dan ham)",
+            "BBQ Chicken",
+            "Vegetarian",
+            "Meat Lovers",
+            "Buffalo Chicken",
+            "Quattro Formaggi (empat keju)",
+            "Pesto Chicken",
+            "Supreme (campuran berbagai topping)"});
+            this.comboBoxRasa.Location = new System.Drawing.Point(338, 178);
+            this.comboBoxRasa.Name = "comboBoxRasa";
+            this.comboBoxRasa.Size = new System.Drawing.Size(294, 24);
+            this.comboBoxRasa.TabIndex = 11;
+            this.comboBoxRasa.Leave += new System.EventHandler(this.comboBoxRasa_Leave);
+            // 
+            // textBoxPonsel
+            // 
+            this.textBoxPonsel.Location = new System.Drawing.Point(338, 222);
+            this.textBoxPonsel.Name = "textBoxPonsel";
+            this.textBoxPonsel.Size = new System.Drawing.Size(294, 22);
+            this.textBoxPonsel.TabIndex = 12;
+            this.textBoxPonsel.Leave += new System.EventHandler(this.textBoxPonsel_Leave);
+            // 
+            // numericUpDownJumlah
+            // 
+            this.numericUpDownJumlah.Location = new System.Drawing.Point(338, 135);
+            this.numericUpDownJumlah.Name = "numericUpDownJumlah";
+            this.numericUpDownJumlah.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDownJumlah.TabIndex = 13;
+            this.numericUpDownJumlah.Leave += new System.EventHandler(this.numericUpDownJumlah_Leave);
+            // 
+            // textBoxEmail
+            // 
+            this.textBoxEmail.Location = new System.Drawing.Point(338, 269);
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.Size = new System.Drawing.Size(294, 22);
+            this.textBoxEmail.TabIndex = 14;
+            this.textBoxEmail.Leave += new System.EventHandler(this.textBoxEmail_Leave);
+            // 
+            // textBoxAlamat
+            // 
+            this.textBoxAlamat.Location = new System.Drawing.Point(338, 367);
+            this.textBoxAlamat.Name = "textBoxAlamat";
+            this.textBoxAlamat.Size = new System.Drawing.Size(294, 22);
+            this.textBoxAlamat.TabIndex = 15;
+            this.textBoxAlamat.Leave += new System.EventHandler(this.textBoxAlamat_Leave);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(124, 315);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(93, 16);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Code Promo";
+            // 
+            // textBoxPromo
+            // 
+            this.textBoxPromo.Location = new System.Drawing.Point(338, 315);
+            this.textBoxPromo.Name = "textBoxPromo";
+            this.textBoxPromo.Size = new System.Drawing.Size(294, 22);
+            this.textBoxPromo.TabIndex = 17;
+            this.textBoxPromo.Leave += new System.EventHandler(this.textBoxPromo_Leave);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 527);
+            this.Controls.Add(this.textBoxPromo);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBoxAlamat);
+            this.Controls.Add(this.textBoxEmail);
+            this.Controls.Add(this.numericUpDownJumlah);
+            this.Controls.Add(this.textBoxPonsel);
+            this.Controls.Add(this.comboBoxRasa);
+            this.Controls.Add(this.textBoxNama);
+            this.Controls.Add(this.buttonBatal);
+            this.Controls.Add(this.buttonPesan);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name = "Form1";
+            this.Text = "Pizza";
+            ((System.ComponentModel.ISupportInitialize)(this.epCorrect)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epWarning)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epWrong)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownJumlah)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button buttonPesan;
+        private System.Windows.Forms.Button buttonBatal;
+        private System.Windows.Forms.TextBox textBoxNama;
+        private System.Windows.Forms.ErrorProvider epCorrect;
+        private System.Windows.Forms.ErrorProvider epWarning;
+        private System.Windows.Forms.ErrorProvider epWrong;
+        private System.Windows.Forms.ComboBox comboBoxRasa;
+        private System.Windows.Forms.TextBox textBoxEmail;
+        private System.Windows.Forms.NumericUpDown numericUpDownJumlah;
+        private System.Windows.Forms.TextBox textBoxPonsel;
+        private System.Windows.Forms.TextBox textBoxAlamat;
+        private System.Windows.Forms.TextBox textBoxPromo;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Pertemuan06/P6_4_714220043/P6_ 4_714220043/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P6__4_714220043
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBoxNama_Leave(object sender, EventArgs e)
+        {
+            if (textBoxNama.Text == "")
+            {
+                epCorrect.SetError(textBoxNama, "");
+                epWarning.SetError(textBoxNama, "Nama harus di isi, Tidak boleh kosong");
+                epWrong.SetError(textBoxNama, ""); ;
+            }
+            else
+            {
+                if ((textBoxNama.Text).All(Char.IsLetter))
+                {
+                    epCorrect.SetError(textBoxNama, "benar");
+                    epWarning.SetError(textBoxNama, "");
+                    epWrong.SetError(textBoxNama, "");
+                }
+                else
+                {
+                    epCorrect.SetError(textBoxNama, "");
+                    epWarning.SetError(textBoxNama, "");
+                    epWrong.SetError(textBoxNama, "Nama hanya boleh berisi karakter huruf");
+                }
+            }
+        }
+
+        private void numericUpDownJumlah_Leave(object sender, EventArgs e)
+        {
+            int tiket = (int)numericUpDownJumlah.Value;
+
+            if (tiket > 0 && tiket <= 5)
+            {
+                epCorrect.SetError(numericUpDownJumlah, "benar");
+                epWarning.SetError(numericUpDownJumlah, "");
+                epWrong.SetError(numericUpDownJumlah, "");
+            }
+            else if (tiket > 5)
+            {
+                epCorrect.SetError(numericUpDownJumlah, "");
+                epWarning.SetError(numericUpDownJumlah, "Pesanan tidak boleh lebih dari 5");
+                epWrong.SetError(numericUpDownJumlah, "");
+            }
+            else if (tiket == 0)
+            {
+                epCorrect.SetError(numericUpDownJumlah, "");
+                epWarning.SetError(numericUpDownJumlah, "");
+                epWrong.SetError(numericUpDownJumlah, "Pesanan tidak boleh 0");
+            }
+        }
+
+        private void comboBoxRasa_Leave(object sender, EventArgs e)
+        {
+            if (comboBoxRasa.SelectedItem != null)
+            {
+                string pilihRasa = comboBoxRasa.SelectedItem.ToString();
+                epCorrect.SetError(comboBoxRasa, "Rasa Terpilih");
+                epWarning.SetError(comboBoxRasa, "");
+                epWrong.SetError(comboBoxRasa, "");
+            }
+            else if (string.IsNullOrWhiteSpace(comboBoxRasa.Text))
+            {
+                epCorrect.SetError(comboBoxRasa, "");
+                epWarning.SetError(comboBoxRasa, "Pilih salah satu rasa");
+                epWrong.SetError(comboBoxRasa, "");
+            }
+            else
+            {
+                epCorrect.SetError(comboBoxRasa, "");
+                epWarning.SetError(comboBoxRasa, "");
+                epWrong.SetError(comboBoxRasa, "Rasa Tidak Boleh Lain dari pilihan dan berbentuk angka");
+            }
+        }
+
+        private void textBoxPonsel_Leave(object sender, EventArgs e)
+        {
+            string ponsel = textBoxPonsel.Text;
+
+            if (!string.IsNullOrWhiteSpace(ponsel))
+            {
+                if (ponsel.Length >= 10 && ponsel.Length <= 12)
+                {
+                    epCorrect.SetError(textBoxPonsel, "Correct");
+                    epWarning.SetError(textBoxPonsel, "");
+                    epWrong.SetError(textBoxPonsel, "");
+                }
+                else if (ponsel.Length < 10)
+                {
+                    epCorrect.SetError(textBoxPonsel, "");
+                    epWarning.SetError(textBoxPonsel, "Minimal 10 digit");
+                    epWrong.SetError(textBoxPonsel, "");
+                }
+                else if (ponsel.Length > 12)
+                {
+                    epCorrect.SetError(textBoxPonsel, "");
+                    epWarning.SetError(textBoxPonsel, "Maksimal 12 digit");
+                    epWrong.SetError(textBoxPonsel, "");
+                }
+            }
+
+            else
+            {
+                epCorrect.SetError(textBoxPonsel, "");
+                epWarning.SetError(textBoxPonsel, "");
+                epWrong.SetError(textBoxPonsel, "Nomor ponsel tidak boleh kosong.");
+            }
+        }
+
+        private void textBoxEmail_Leave(object sender, EventArgs e)
+        {
+            if (Regex.IsMatch(textBoxEmail.Text, @"^^[^@\s]+@[^@\s]+(\.[^@\s]+)+$"))
+            {
+                epCorrect.SetError(textBoxEmail, "Correct");
+                epWarning.SetError(textBoxEmail, "");
+                epWrong.SetError(textBoxEmail, "");
+            }
+            else
+            {
+                epCorrect.SetError(textBoxEmail, "");
+                epWarning.SetError(textBoxEmail, "");
+                epWrong.SetError(textBoxEmail, "Format email salah!");
+            }
+            if (textBoxEmail.Text == "")
+            {
+                epCorrect.SetError(textBoxEmail, "");
+                epWarning.SetError(textBoxEmail, "eamil wajib diisi!");
+                epWrong.SetError(textBoxEmail, "");
+            }
+        }
+
+        private void textBoxAlamat_Leave(object sender, EventArgs e)
+        {
+            if ((textBoxAlamat.Text).All(Char.IsUpper))
+            {
+                epCorrect.SetError(textBoxAlamat, "Betul!");
+                epWarning.SetError(textBoxAlamat, "");
+                epWrong.SetError(textBoxAlamat, "");
+            }
+            if ((textBoxAlamat.Text).Any(Char.IsLower))
+            {
+                epCorrect.SetError(textBoxAlamat, "");
+                epWarning.SetError(textBoxAlamat, "");
+                epWrong.SetError(textBoxAlamat, "Alamat Harus Huruf Besar");
+            }
+
+            if ((textBoxAlamat.Text == ""))
+            {
+                epCorrect.SetError(textBoxAlamat, "");
+                epWarning.SetError(textBoxAlamat, "Alamat Tidak Boleh Kosong");
+                epWrong.SetError(textBoxAlamat, "");
+            }
+        }
+
+        private void textBoxPromo_Leave(object sender, EventArgs e)
+        {
+            if ((textBoxPromo.Text).All(Char.IsLower))
+            {
+                epCorrect.SetError(textBoxPromo, "Betul!");
+                epWarning.SetError(textBoxPromo, "");
+                epWrong.SetError(textBoxPromo, "");
+            }
+            if ((textBoxPromo.Text).Any(Char.IsUpper))
+            {
+                epCorrect.SetError(textBoxPromo, "");
+                epWarning.SetError(textBoxPromo, "");
+                epWrong.SetError(textBoxPromo, "Gunakan Huruf kecil'");
+            }
+
+            if ((textBoxPromo.Text == ""))
+            {
+                epCorrect.SetError(textBoxPromo, "");
+                epWarning.SetError(textBoxPromo, "Huruf jangan Kosong");
+                epWrong.SetError(textBoxPromo, "");
+            }
+        }
+
+
+        private void buttonBatal_Click_1(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void buttonPesan_Click(object sender, EventArgs e)
+        {
+            string nama = textBoxNama.Text;
+            int jumlahTiket = (int)numericUpDownJumlah.Value;
+            string rasa = comboBoxRasa.SelectedItem.ToString(); 
+            string nomorPonsel = textBoxPonsel.Text;
+            string email = textBoxEmail.Text;
+            string promo = textBoxPromo.Text;
+            string alamat = textBoxAlamat.Text;
+
+
+            string hasilPesan = $"Nama: {nama}\nJumlah Tiket: {jumlahTiket}\nRasa: {rasa}\nNomor Ponsel: {nomorPonsel}\nEmail: {email}\nPromo: {promo}\nAlamat: {alamat}";
+
+            MessageBox.Show(hasilPesan, "Detail Pesanan", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
