18913cbf85820ae0d0f929ff9ae25b1e173f93b9
Yoginara Pratama Sitorus <114574855+Yoginara@users.noreply.github.com>
2023-12-06 14:46:28 +0700

    Pertemuan09

---
+++ Pertemuan09/P9_714220043/P9_714220043/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P9_714220043
+{
+    public partial class Form1 : Form
+    {
+        Koneksi koneksi = new Koneksi();
+
+        public void Tampil()
+        {
+            //Query DB Get MGS
+            DataMahasiswa.DataSource = koneksi.ShowData("SELECT * FROM t_mahasiswa");
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            // Memanggil Method tampil
+            Tampil();
+        }
+    }
+}
