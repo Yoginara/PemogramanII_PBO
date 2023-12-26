2e4f58ef606c47ad6b9a1e252d02492a4bf62d32
Yoginara Pratama Sitorus <114574855+Yoginara@users.noreply.github.com>
2023-12-27 00:20:38 +0700

    Pertemuan12

---
+++ Pertemuan12/P9_2_714220043/P9_714220043/view/Form1.cs
@@ -0,0 +1,140 @@
+﻿using P9_714220043.controller;
+using P9_714220043.model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P9_714220043
+{
+    public partial class Form1 : Form
+    {
+        Koneksi koneksi = new Koneksi();
+        M_mahasiswa m_mhs = new M_mahasiswa();
+
+        public void Tampil()
+        {
+            //Query DB Get MGS
+            DataMahasiswa.DataSource = koneksi.ShowData("SELECT * FROM t_mahasiswa");
+            DataMahasiswa.Columns[0].HeaderText = "NPM";
+            DataMahasiswa.Columns[1].HeaderText = "Nama";
+            DataMahasiswa.Columns[2].HeaderText = "Angkatan";
+            DataMahasiswa.Columns[3].HeaderText = "Alamat";
+            DataMahasiswa.Columns[4].HeaderText = "Email";
+            DataMahasiswa.Columns[5].HeaderText = "No HP";
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void ResetForm()
+        {
+            npm.Text = "";
+            nama.Text = "";
+            angkatan.SelectedIndex = -1;
+            alamat.Text = "";
+            email.Text = "";
+            nohp.Text = "";
+            tbCariData.Text = "";
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            // Memanggil Method tampil
+            Tampil();
+        }
+
+        private void btnSimpan_Click(object sender, EventArgs e)
+        {
+            if (npm.Text == "" || nama.Text == "" || angkatan.SelectedIndex == -1 || alamat.Text == "" || email.Text == "" || nohp.Text == "")
+            {
+                MessageBox.Show("Data tidak boleh kosong", "Peringatan",
+                MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                Mahasiswa mhs = new Mahasiswa();
+                m_mhs.Npm = npm.Text;
+                m_mhs.Nama = nama.Text;
+                m_mhs.Angkatan = angkatan.Text;
+                m_mhs.Alamat = alamat.Text;
+                m_mhs.Email = email.Text;
+                m_mhs.Nohp = nohp.Text;
+
+                mhs.Insert(m_mhs);
+
+                ResetForm();
+                Tampil();
+            }
+        }
+        private void DataMahasiswa_CellClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+            npm.Text = DataMahasiswa.Rows[e.RowIndex].Cells[0].Value.ToString();
+            nama.Text = DataMahasiswa.Rows[e.RowIndex].Cells[1].Value.ToString();
+            angkatan.Text = DataMahasiswa.Rows[e.RowIndex].Cells[2].Value.ToString();
+            alamat.Text = DataMahasiswa.Rows[e.RowIndex].Cells[3].Value.ToString();
+            email.Text = DataMahasiswa.Rows[e.RowIndex].Cells[4].Value.ToString();
+            nohp.Text = DataMahasiswa.Rows[e.RowIndex].Cells[5].Value.ToString();
+        }
+
+        private void btnUbah_Click(object sender, EventArgs e)
+        {
+            if (npm.Text == "" || nama.Text == "" || angkatan.SelectedIndex == -1 || alamat.Text == "" || email.Text == "" || nohp.Text == "")
+            {
+                MessageBox.Show("Data tidak boleh kosong", "Peringatan",
+                MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                Mahasiswa mhs = new Mahasiswa();
+                m_mhs.Npm = npm.Text;
+                m_mhs.Nama = nama.Text;
+                m_mhs.Angkatan = angkatan.Text;
+                m_mhs.Alamat = alamat.Text;
+                m_mhs.Email = email.Text;
+                m_mhs.Nohp = nohp.Text;
+
+                mhs.Update(m_mhs, npm.Text);
+
+                ResetForm();
+                Tampil();
+            }
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            ResetForm();
+            Tampil();
+        }
+
+        private void btnHapus_Click(object sender, EventArgs e)
+        {
+            DialogResult pesan = MessageBox.Show(
+                "Apakah yakin akan menghapus data ini?",
+                "Perhatian",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (pesan == DialogResult.Yes)
+            {
+                Mahasiswa mhs = new Mahasiswa();
+                mhs.Delete(npm.Text);
+
+                ResetForm();
+                Tampil();
+            }
+        }
+
+        private void tbCariData_TextChanged(object sender, EventArgs e)
+        {
+            //Query DB Search Data
+            DataMahasiswa.DataSource = koneksi.ShowData("SELECT * FROM t_mahasiswa WHERE npm LIKE '%' '" + tbCariData.Text + "' '%' OR nama LIKE '%' '" + tbCariData.Text + "' '%' ");
+        }
+    }
+    
+}
---
+++ Pertemuan12/P9_2_714220043/P9_714220043/view/ParentForm.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace P9_714220043.view
+{
+    partial class ParentForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MdiMenu = new System.Windows.Forms.MenuStrip();
+            this.FileMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataMahasiswaItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataNilaiItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tugasPratikum12ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataMasterBarangToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataTransaksiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.MdiMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // MdiMenu
+            // 
+            this.MdiMenu.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.MdiMenu.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.MdiMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.FileMenuItem,
+            this.dataMahasiswaItem,
+            this.dataNilaiItem,
+            this.tugasPratikum12ToolStripMenuItem});
+            this.MdiMenu.Location = new System.Drawing.Point(0, 0);
+            this.MdiMenu.Name = "MdiMenu";
+            this.MdiMenu.Size = new System.Drawing.Size(800, 28);
+            this.MdiMenu.TabIndex = 1;
+            this.MdiMenu.Text = "menuStrip1";
+            // 
+            // FileMenuItem
+            // 
+            this.FileMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.FileMenuItem.Name = "FileMenuItem";
+            this.FileMenuItem.Size = new System.Drawing.Size(46, 24);
+            this.FileMenuItem.Text = "&File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(116, 26);
+            this.exitToolStripMenuItem.Text = "&Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // dataMahasiswaItem
+            // 
+            this.dataMahasiswaItem.Name = "dataMahasiswaItem";
+            this.dataMahasiswaItem.Size = new System.Drawing.Size(131, 24);
+            this.dataMahasiswaItem.Text = "Data Mahasiswa";
+            this.dataMahasiswaItem.Click += new System.EventHandler(this.dataMahasiswaItem_Click);
+            // 
+            // dataNilaiItem
+            // 
+            this.dataNilaiItem.Name = "dataNilaiItem";
+            this.dataNilaiItem.Size = new System.Drawing.Size(90, 24);
+            this.dataNilaiItem.Text = "Data Nilai";
+            this.dataNilaiItem.Click += new System.EventHandler(this.dataNilaiItem_Click);
+            // 
+            // tugasPratikum12ToolStripMenuItem
+            // 
+            this.tugasPratikum12ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dataMasterBarangToolStripMenuItem,
+            this.dataTransaksiToolStripMenuItem});
+            this.tugasPratikum12ToolStripMenuItem.Name = "tugasPratikum12ToolStripMenuItem";
+            this.tugasPratikum12ToolStripMenuItem.Size = new System.Drawing.Size(144, 24);
+            this.tugasPratikum12ToolStripMenuItem.Text = "Tugas Pratikum 12";
+            // 
+            // dataMasterBarangToolStripMenuItem
+            // 
+            this.dataMasterBarangToolStripMenuItem.Name = "dataMasterBarangToolStripMenuItem";
+            this.dataMasterBarangToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.dataMasterBarangToolStripMenuItem.Text = "Data Master Barang";
+            this.dataMasterBarangToolStripMenuItem.Click += new System.EventHandler(this.dataMasterBarangToolStripMenuItem_Click);
+            // 
+            // dataTransaksiToolStripMenuItem
+            // 
+            this.dataTransaksiToolStripMenuItem.Name = "dataTransaksiToolStripMenuItem";
+            this.dataTransaksiToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.dataTransaksiToolStripMenuItem.Text = "Data Transaksi";
+            this.dataTransaksiToolStripMenuItem.Click += new System.EventHandler(this.dataTransaksiToolStripMenuItem_Click);
+            // 
+            // ParentForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.MdiMenu);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.MdiMenu;
+            this.Name = "ParentForm";
+            this.Text = "ParentForm";
+            this.MdiMenu.ResumeLayout(false);
+            this.MdiMenu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip MdiMenu;
+        private System.Windows.Forms.ToolStripMenuItem FileMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dataMahasiswaItem;
+        private System.Windows.Forms.ToolStripMenuItem dataNilaiItem;
+        private System.Windows.Forms.ToolStripMenuItem tugasPratikum12ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dataMasterBarangToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dataTransaksiToolStripMenuItem;
+    }
+}
